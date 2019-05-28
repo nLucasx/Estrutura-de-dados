a044daff749c5eabc95024880a1375f6d657ae65
Lucas Buarque <47061990+nLucasx@users.noreply.github.com>
2019-05-27 21:42:41 -0300

    Ex: Ponteiro pra função. MAP

---
+++ map.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+int factorial(int x)
+{
+	if (x < 0)
+	{
+		return -1;
+	}
+	else if (x == 0 || x == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return x * factorial(x-1);
+	}
+}
+int square(int x)
+{
+	return x * x;
+}
+void map(int (*function)(int x), int *vector, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", (*function)(vector[i]));
+	}
+	printf("\n");
+}
+int main(){
+	int n, *vector,i;
+
+	printf("Type the size of your array >> ");
+	scanf("%d", &n);
+	printf("Type your numbers >> ");
+	vector = malloc(n * sizeof(int));
+	
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &vector[i]);
+	}
+
+	printf("Factorials of the numbers in your array:\n");
+	map(factorial,vector,n);
+
+	printf("The square of numbers in your array:\n");
+	map(square,vector,n);
+
+
+
+}
