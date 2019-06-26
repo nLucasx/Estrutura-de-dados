5ecce6ae2189997641e824296181ab6b49c9b1d1
Lucas Buarque <47061990+nLucasx@users.noreply.github.com>
2019-06-25 22:11:26 -0300

    Ex: Lista Encadeada - Cadastro/Busca de alunos

--- cadastro.c
+++ cadastro.c
@@ -20,6 +20,10 @@ dados * cadastra_aluno(dados * cabeca, char aluno[40], int idadealuno, char sexo
 }
 void mostra_alunos(dados *cabeca)
 {
+	if (cabeca == NULL)
+	{
+	    printf("A lista esta vazia\n");
+	}
 	while (cabeca != NULL)
 	{
 		printf("Aluno: %s", cabeca->nome);
@@ -38,10 +42,14 @@ void imprime_aluno(int flag, dados *cabeca)
 		printf("Cpf: %s\n", cabeca->cpf);
 		printf("Sexo: %c\n\n", cabeca->sexo);            
     }
-    else printf("Aluno nao encontrado");
+    else printf("Aluno nao encontrado\n");
 }
 int existe_aluno(char aluno[40], dados *cabeca)
 {
+	if (cabeca == NULL)
+	{
+	    return 0;
+	}
 	if (strcasecmp(cabeca->nome, aluno) == 0)
 	{
 		return 1;
@@ -62,19 +70,14 @@ dados * retorna_posicao(char aluno[40], dados *cabeca)
 }
 void apaga_aluno(char aluno[40], dados *cabeca)
 {
-	if (existe_aluno(aluno,cabeca))
-	{
-		cabeca = retorna_posicao(aluno,cabeca);
-		dados *auxiliar;
-		auxiliar = cabeca->proximo;
-		cabeca->proximo = auxiliar->proximo;
-		free(auxiliar);
-		printf("Aluno apagado com sucesso!\n");
-	}
-	else
-	{
-		printf("O Aluno nao esta cadastrado!\n");
-	}
+	cabeca = retorna_posicao(aluno,cabeca);
+	dados *auxiliar;
+	auxiliar = cabeca->proximo;
+	cabeca->proximo = auxiliar->proximo;
+	free(auxiliar);
+	printf("Cadastro apagado com sucesso!\n");
+	
+	
 }
 int main()
 {
@@ -101,10 +104,13 @@ int main()
 	    cadastra = cadastra_aluno(cadastra, aluno, idade, sexo, cpf);
 	}
     system("clear");
+    printf("Aluno(s) cadastrado(s) com sucesso!\n");
+    inicio:
     printf("O que deseja?\n");
     printf("[1] Buscar um aluno\n");
     printf("[2] Mostrar todos os alunos\n");
     printf("[3] Apagar um aluno cadastrado\n");
+    printf("[4] Cadastrar mais alunos\n");
     scanf("%d", &escolha);
     getchar();
     system("clear");
@@ -115,11 +121,35 @@ int main()
         fgets(aluno,40,stdin);
         system("clear");
         imprime_aluno(existe_aluno(aluno,cadastra),cadastra);
+        printf("Deseja fazer outra operacao?\n");
+        printf("[1] SIM\n");
+        printf("[2] NAO\n");
+        scanf("%d", &escolha);
+        if (escolha == 1)
+        {
+            goto inicio;
+        }
+        else
+        {
+            printf("Saindo...\n\n");
+        }
     }
     else if (escolha == 2)
     {
         system("clear");
         mostra_alunos(cadastra);
+        printf("Deseja fazer outra operacao?\n");
+        printf("[1] SIM\n");
+        printf("[2] NAO\n");
+        scanf("%d", &escolha);
+        if (escolha == 1)
+        {
+            goto inicio;
+        }
+        else
+        {
+            printf("Saindo...\n\n");
+        }
     }
     else if (escolha == 3)
     {
@@ -127,15 +157,95 @@ int main()
     	fgets(aluno,40,stdin);
     	system("clear");
     	int existe = existe_aluno(aluno, cadastra);
-    	apaga_aluno(aluno, cadastra);
     	if (existe)
     	{
-    		mostra_alunos(cadastra);	
+    		if (cadastra->proximo == NULL)
+    		{
+    		    cadastra = NULL;
+    		    printf("Cadastro apagado com sucesso!\n");
+    		    mostra_alunos(cadastra);
+    		}
+    		else if (strcasecmp(cadastra->nome, aluno) == 0)
+    		{
+    		    dados *auxiliar = cadastra;
+    		    cadastra = cadastra->proximo;
+    		    free(auxiliar);
+    		    mostra_alunos(cadastra);
+    		}
+    		else
+    		{
+    		    apaga_aluno(aluno, cadastra);
+    		    mostra_alunos(cadastra);
+    		}
     	}
+    	else
+    	{
+    	    printf("O Aluno nao esta cadastrado!\n");
+    	}
+        
+        printf("Deseja fazer outra operacao?\n");
+        printf("[1] SIM\n");
+        printf("[2] NAO\n");
+        scanf("%d", &escolha);
+        if (escolha == 1)
+        {
+            goto inicio;
+        }
+        else
+        {
+            printf("Saindo...\n\n");
+        }
+    }
+    else if (escolha == 4)
+    {
+        printf("Quantos alunos voce quer cadastrar?\n");
+	    scanf("%d", &qt);
+	    getchar();
+	
+	    for (i = 0; i < qt; i++)
+	    {
+	        system("clear");
+	        printf("Digite o nome >> ");
+	        fgets(aluno,40,stdin);
+	        printf("Digite a idade >> ");
+	        scanf("%d", &idade);
+	        printf("Digite o cpf >>");
+	        scanf(" %s", cpf);
+	        printf("Digite o sexo >> ");
+	        scanf(" %c", &sexo);
+	        getchar();
+	    
+	        cadastra = cadastra_aluno(cadastra, aluno, idade, sexo, cpf);
+	    }
+	    printf("Aluno(s) cadastrado(s) com sucesso!\n");
+        printf("Deseja fazer outra operacao?\n");
+        printf("[1] SIM\n");
+        printf("[2] NAO\n");
+        scanf("%d", &escolha);
+        if (escolha == 1)
+        {
+            goto inicio;
+        }
+        else
+        {
+            printf("Saindo...\n\n");
+        }
     }
     else
     {
-        printf("Saindo...\n\n");
+        system("clear");
+        printf("Deseja fazer outra operacao?\n");
+        printf("[1] SIM\n");
+        printf("[2] NAO\n");
+        scanf("%d", &escolha);
+        if (escolha == 1)
+        {
+            goto inicio;
+        }
+        else
+        {
+            printf("Saindo...\n\n");
+        }
     }
 
 	return 0;
